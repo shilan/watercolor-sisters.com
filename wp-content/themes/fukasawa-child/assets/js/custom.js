dc5d7920ff1ed8cdbcae286e7f86c41e839b5ea9
Shilan <kalhor@gmail.com>
2022-12-09 15:12:51 -0600

    full push

---
+++ wp-content/themes/fukasawa-child/assets/js/custom.js
@@ -0,0 +1,33 @@
+jQuery( document ).ready( function( $ ) {
+    
+$('div[expand]').addClass('img-enlargeable').click(function() {
+    var src = $('figure[data-enlargeable] img').attr('src');
+    var modal;
+  
+    function removeModal() {
+      modal.remove();
+      $('body').off('keyup.modal-close');
+    }
+    modal = $('<div>').css({
+      background: 'RGBA(0,0,0,.8) url(' + src + ') no-repeat center',
+      backgroundSize: 'contain',
+      width: '100%',
+      height: '100%',
+      position: 'fixed',
+      zIndex: '10000',
+      top: '0',
+      left: '0',
+      cursor: 'zoom-out'
+    }).click(function() {
+      removeModal();
+    }).appendTo('body');
+    
+    //handling ESC
+    $('body').on('keyup.modal-close', function(e) {
+      if (e.key === 'Escape') {
+        removeModal();
+      }
+    });
+  });
+
+} );
